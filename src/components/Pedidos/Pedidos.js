60946fe14cf5d959040845700a72abb7ca20752d
VituAmaral <victorflorindo9@gmail.com>
2020-06-18 15:07:57 -0300

    listando pedidos

---
+++ src/components/Pedidos/Pedidos.js
@@ -0,0 +1,162 @@
+import React, {Component, useReducer, useState} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Table, Image, ButtonGroup, Button} from 'react-bootstrap'
+import './style.css'
+import api from '../../services/api'
+
+/*
+Listagem de Pedidos
+Victor F. Amaral
+*/
+
+ class Pedidos extends Component{
+
+ state = {
+     pedidos: []
+ }
+
+ componentDidMount () {
+    api.get('/orders')
+    .then(response => {
+        this.setState({
+            pedidos:response.data
+        })
+        
+    })
+    
+}
+
+componentWillMount(){
+    
+}
+
+
+
+
+
+
+    render () {
+        var listaPedidos = this.state.pedidos;
+        return (
+            
+            <div className="mb-3 card">
+                <div className="card-header">
+                    <div className="alignm-items-center">
+                        <div className="col">
+                            <h5>Pedidos</h5>
+                        </div>
+                        <div className="text-right col-auto">
+                        </div>
+                    </div>
+                </div>
+                <div className="p-0 card-body">
+                    <div className="table-responsive">
+                        <div className="react-bootstrap-table">
+                            <Table className="table table-dashboard table-striped table-sm fs--1 border-bottom mb-0 table-dashboard-th-nowrap">
+                                <thead>
+                                    <tr className="bg-200 text-900">
+                                        <th className="selection-cell-header" data-row-selection="true">
+                                            <div class="custom-control custom-checkbox">
+                                                <input class="custom-control-input" type="checkbox"/>
+                                                <label class="custom-control-label"/>
+                                                    
+                                            </div>
+                                        </th>
+                                        <th tabIndex="0" arial-label="Order sortable" className="sortable">
+                                            Pedidos
+                                            <span className="order-4">
+                                                
+                                            </span>
+                                        </th>
+                                        <th tabIndex="0" arial-label="Data sortable" className="sortable">
+                                            Data
+                                            <span className="order-4">
+                                                
+                                            </span>
+                                        </th>
+                                        <th tabIndex="0" arial-label="Data sortable" className="sortable">
+                                            Endereço do vendedor
+                                            <span className="order-4">
+                                                
+                                            </span>
+                                        </th>
+                                        <th tabIndex="0" arial-label="Data sortable" className="sortable">
+                                            Status
+                                            <span className="order-4">
+                                                
+                                            </span>
+                                        </th>
+                                        <th tabIndex="0" arial-label="Data sortable" className="sortable">
+                                            Quantidade
+                                            <span className="order-4">
+                                                
+                                            </span>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {listaPedidos.length === 0 ?
+                                        <h1>Teste</h1>:
+
+                                    listaPedidos.map(pedido =>(
+                                        
+                                    <tr className="btn-reveal-trigger" key={pedido.id}>
+                                        
+                                        <td className="selection-cell">
+                                            <div className="custom-control custom-checkbox">
+                                                <input className="custom-control-input"/>
+                                                <label className="custom-control-label"/>
+                                            </div>
+                                        </td>
+                                        
+                                        <td className="py-2 align-middle">
+                                            <a>
+                                                <strong>
+                                                    {pedido.id}
+                                                </strong>
+                                            </a>                                            
+                                            <br/>
+                                            {/* <a>Email</a> */}
+                                        </td>
+                                        <td className="py-2 align-middle">
+                                            <strong>{ pedido.createDate.split('T')[0] + " " }</strong> 
+                                            { (pedido.createDate.split('T')[1].split('.')[0]) }
+                                        </td>
+                                        <td className="py-2 align-middle">
+                                            {/* {(pedido.company.phones.map(phone => (
+                                                phone.number + ", "
+                                            )))} */}
+                                            {listaPedidos[0].company.publicPlace}{" "}
+                                            {listaPedidos[0].company.address}{" "}
+                                            {listaPedidos[0].company.neighborhood}{" "}
+                                            {listaPedidos[0].company.number}{" "}
+                                            
+                                        </td>
+                                        <td className="py-2 align-middle">
+                                            <span className="d-block p-4 badge badge-soft-success">
+                                                {pedido.status}
+                                                <sgv>
+                                                    
+                                                </sgv>
+                                            </span>
+                                        </td>
+                                        
+                                        <td className="py-2 align-middle">
+                                            
+                                        </td>
+                                        
+                                        
+                                    </tr>
+                                    ) ).reverse()
+                                    }
+                                </tbody>
+                            </Table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            )
+    }
+}
+
+export default Pedidos
