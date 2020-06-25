d63d26a9e3c32550252df1f13d349d13e054445d
VituAmaral <victorflorindo9@gmail.com>
2020-06-25 14:25:13 -0300

    modal de Pedidos

--- src/components/Pedidos/Pedidos.js
+++ src/components/Pedidos/Pedidos.js
@@ -1,180 +1,231 @@
-import React, { Component, useReducer, useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Table, Image, ButtonGroup, Button } from 'react-bootstrap'
-import './style.css'
-import api from '../../services/api'
-import Modal from './modalPedidos'
+import React, { Component, useReducer, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Table, Image, ButtonGroup, Button } from "react-bootstrap";
+import "./style.css";
+import api from "../../services/api";
 
 /*
 Listagem de Pedidos
 Victor F. Amaral
 */
 
+const ModalPedidos = (props) => {
+  return (
+    <div
+      className="modal"
+      tabindex="9"
+      role="dialog"
+      style={{ display: "block" }}
+    >
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">Modal title</h5>
+            <button
+              type="button"
+              className="close"
+              data-dismiss="modal"
+              aria-label="Close"
+              onClick={() => {
+                props.showModal();
+              }}
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body">
+            <p>Modal body text goes here.</p>
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-primary">
+              Save changes
+            </button>
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-dismiss="modal"
+              onClick={() => {
+                props.showModal();
+              }}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 class Pedidos extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            showModal: false,
-            pedidos : [],    
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+      pedidos: []
     }
+  }
 
-    componentDidMount() {
-        api.get('/orders')
-            .then(response => {
-                this.setState({
-                    pedidos: response.data
-                })
+  componentDidMount() {
+    api.get("/orders").then((response) => {
+      this.setState({
+        pedidos: []
+      });
+    });
+  }
 
-            })
+  // chamarModal = () => {
+  //     this.setState({
+  //         isShowing: true
+  //     });
+  // }
 
-    }
+  // fecharModal = () => {
+  //     this.setState({
+  //         isShowing: false
+  //     });
+  // }
 
-    // chamarModal = () => {
-    //     this.setState({
-    //         isShowing: true
-    //     });
-    // }
+  showModal = () => {
+    this.setState({ showModal: !this.state.showModal });
+  };
 
-    // fecharModal = () => {
-    //     this.setState({
-    //         isShowing: false
-    //     });
-    // }
+  render() {
+    var listaPedidos = this.state.pedidos;
+    return (
+      <div className="mb-3 card">
+        <div className="card-header">
+          <div className="alignm-items-center">
+            <div className="col">
+              <h5>Pedidos</h5>
+            </div>
+            <div className="text-right col-auto"></div>
+          </div>
+        </div>
+        <div className="p-0 card-body">
+          <div className="table-responsive">
+            <div className="react-bootstrap-table">
+              <Table className="table table-dashboard table-striped table-sm fs--1 border-bottom mb-0 table-dashboard-th-nowrap">
+                <thead>
+                  <tr className="bg-200 text-900">
+                    <th
+                      className="selection-cell-header"
+                      data-row-selection="true"
+                    >
+                      <div class="custom-control custom-checkbox">
+                        <input class="custom-control-input" type="checkbox" />
+                        <label class="custom-control-label" />
+                      </div>
+                    </th>
+                    <th
+                      tabIndex="0"
+                      arial-label="Order sortable"
+                      className="sortable"
+                    >
+                      Pedidos
+                      <span className="order-4"></span>
+                    </th>
+                    <th
+                      tabIndex="0"
+                      arial-label="Data sortable"
+                      className="sortable"
+                    >
+                      Data
+                      <span className="order-4"></span>
+                    </th>
+                    <th
+                      tabIndex="0"
+                      arial-label="Data sortable"
+                      className="sortable"
+                    >
+                      Preço
+                      <span className="order-4"></span>
+                    </th>
+                    <th
+                      tabIndex="0"
+                      arial-label="Data sortable"
+                      className="sortable"
+                    >
+                      Status
+                      <span className="order-4"></span>
+                    </th>
+                    <th
+                      tabIndex="0"
+                      arial-label="Data sortable"
+                      className="sortable"
+                    >
+                      Quantidade
+                      <span className="order-4"></span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {listaPedidos.length === 0 ? (
+                    <h1>Teste</h1>
+                  ) : (
+                    listaPedidos
+                      .map((pedido) => (
+                        <tr className="btn-reveal-trigger" key={pedido.id}>
+                          <td className="selection-cell">
+                            <div className="custom-control custom-checkbox">
+                              <input className="custom-control-input" />
+                              <label className="custom-control-label" />
+                            </div>
+                          </td>
 
-
-
-
-
-
-    render() {
-        var listaPedidos = this.state.pedidos;
-        return (
-
-            <div className="mb-3 card">
-                <div className="card-header">
-                    <div className="alignm-items-center">
-                        <div className="col">
-                            <h5>Pedidos</h5>
-                        </div>
-                        <div className="text-right col-auto">
-                        </div>
-                    </div>
-                </div>
-                <div className="p-0 card-body">
-                    <div className="table-responsive">
-                        <div className="react-bootstrap-table">
-                            <Table className="table table-dashboard table-striped table-sm fs--1 border-bottom mb-0 table-dashboard-th-nowrap">
-                                <thead>
-                                    <tr className="bg-200 text-900">
-                                        <th className="selection-cell-header" data-row-selection="true">
-                                            <div class="custom-control custom-checkbox">
-                                                <input class="custom-control-input" type="checkbox" />
-                                                <label class="custom-control-label" />
-
-                                            </div>
-                                        </th>
-                                        <th tabIndex="0" arial-label="Order sortable" className="sortable">
-                                            Pedidos
-                                            <span className="order-4">
-
-                                            </span>
-                                        </th>
-                                        <th tabIndex="0" arial-label="Data sortable" className="sortable">
-                                            Data
-                                            <span className="order-4">
-
-                                            </span>
-                                        </th>
-                                        <th tabIndex="0" arial-label="Data sortable" className="sortable">
-                                            Preço
-                                            <span className="order-4">
-
-                                            </span>
-                                        </th>
-                                        <th tabIndex="0" arial-label="Data sortable" className="sortable">
-                                            Status
-                                            <span className="order-4">
-
-                                            </span>
-                                        </th>
-                                        <th tabIndex="0" arial-label="Data sortable" className="sortable">
-                                            Quantidade
-                                            <span className="order-4">
-
-                                            </span>
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {listaPedidos.length === 0 ?
-                                        <h1>Teste</h1> :
-
-                                        listaPedidos.map(pedido => (
-
-                                            <tr className="btn-reveal-trigger" key={pedido.id}>
-
-                                                <td className="selection-cell">
-                                                    <div className="custom-control custom-checkbox">
-                                                        <input className="custom-control-input" />
-                                                        <label className="custom-control-label" />
-                                                    </div>
-                                                </td>
-
-                                                <td className="py-2 align-middle">
-                                                    <a>
-                                                        <strong>
-                                                            {pedido.id}
-                                                        </strong>
-                                                    </a>
-                                                    <br />
-                                                </td>
-                                                <td className="py-2 align-middle">
-                                                    <strong>{pedido.createDate.split('T')[0] + " "}</strong>
-                                                    {(pedido.createDate.split('T')[1].split('.')[0])}
-                                                </td>
-                                                <td className="py-2 align-middle">
-                                                    {/* {(pedido.company.phones.map(phone => (
+                          <td className="py-2 align-middle">
+                            <a>
+                              <strong>{pedido.id}</strong>
+                            </a>
+                            <br />
+                          </td>
+                          <td className="py-2 align-middle">
+                            <strong>
+                              {pedido.createDate.split("T")[0] + " "}
+                            </strong>
+                            {pedido.createDate.split("T")[1].split(".")[0]}
+                          </td>
+                          <td className="py-2 align-middle">
+                            {/* {(pedido.company.phones.map(phone => (
                                                 phone.number + ", "
                                             )))} */}
-                                                    
-                                                    {pedido.priceToPay}{" "} 
-
-
-                                                </td>
-                                                <td className="py-2 align-middle">
-                                                    <span className="d-block p-3  badge badge-soft-success">
-                                                        {pedido.status}
-                                                        <sgv>
-
-                                                        </sgv>
-                                                    </span>
-                                                </td>
-                                                    <td className="py-2 align-middle">d
-
-                                                    <button className="btn btn-primary" onClick={() => {
-                                                        this.setState({showModal: !this.state.showModal})
-                                                    }}>Open Modal</button>
-                                                    </td>
-                                                    
-                                            </tr>
-                                        )).reverse()
-                                    }
-                                </tbody>
-                            </Table>
-                            {this.state.showModal && (<Modal
-                                className="modal"
-                                show={this.chamarModal}
-                                close={this.fecharModal}>
-                             </Modal>)}
-                            
-                        </div>
-                    </div>
-                </div>
+                            {pedido.priceToPay}{" "}
+                          </td>
+                          <td className="py-2 align-middle">
+                            <span className="d-block p-3  badge badge-soft-success">
+                              {pedido.status}
+                              <sgv></sgv>
+                            </span>
+                          </td>
+                          <td className="py-2 align-middle">
+                            d
+                            <button
+                              className="btn btn-primary"
+                              onClick={this.showModal}
+                            >
+                              Open Modal
+                            </button>
+                          </td>
+                        </tr>
+                      ))
+                      .reverse()
+                  )}
+                </tbody>
+              </Table>
+              {this.state.showModal && (
+                <ModalPedidos
+                  className="modal"
+                  showModal={this.showModal}
+                  show={this.chamarModal}
+                  close={this.fecharModal}
+                ></ModalPedidos>
+              )}
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Pedidos
+export default Pedidos;
---
+++ src/components/representantes/ListaRepresentantes.js
@@ -0,0 +1,119 @@
+import React, {Component} from 'react'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Table, Image, ButtonGroup, Button} from 'react-bootstrap'
+import foto from "./fts/foto.jpg";
+import axios from 'axios'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faList, faEdit, faTrash} from '@fortawesome/free-solid-svg-icons'
+import api from '../../services/api'
+import { Link, Redirect } from 'react-router-dom'
+
+
+
+/*
+
+Victor F. Amaral
+Listagem e mostra dos representantes
+
+*/
+
+
+export default class ListaRepresentante extends Component {
+
+    
+ constructor(props) {
+        super(props);
+        this.state = {
+            representantes : [],    
+        };
+    }
+
+ componentDidMount() {
+        this.mostrarRepresentantes();
+    }
+
+    mostrarRepresentantes() {
+        api.get('/representatives')
+        .then(response => {
+            this.setState({
+                representantes: response.data
+            })
+        })
+        
+        // axios.get(api)
+        //     .then(response => response.data)
+        //     .then((data) => {
+        //         this.setState({representantes: data})
+                
+        //     })
+    }
+
+    deletar = (id) => {
+        axios.delete("http://localhost:8080/representatives/" + id)
+            .then(response => {
+            if(response.data != null) {
+                alert('representante deletado')
+                this.setState({
+                    representantes: this.state.representantes.filter(representantes =>
+                 representantes.id !== id)
+                })
+            }
+        })
+    }
+
+render() {
+
+
+    return(
+        <Table bordered hover striped variant="dark">
+            <thead>
+                <tr>
+                <th>Nome</th>
+                <th>cpf</th>
+                <th>Email</th>
+                <th>Telefone</th>
+                <th>Editar</th>
+                </tr>
+            </thead>
+            <tbody>
+                {this.state.representantes.length === 0 ?
+                
+                    <tr align="center">
+                        <td colSpan="6"> representantes cadastrados</td>
+                    </tr>:
+                    this.state.representantes.map((representantes) =>(
+                       <tr key={representantes.id}>
+                           <td>
+                                <Image  src={foto} roundedCircle width="25" height="25"/>
+                                {representantes.name}
+                           </td>
+                           <td>
+                                {representantes.cpf}
+                           </td>
+                           <td>
+                                {representantes.email}
+                           </td>
+                           <td>
+                                {/* {representantes.numero} */}
+                           </td>
+                           <td>
+                                <ButtonGroup>
+                                    <Link class="btn btn-sm btn-outline-primary" to={"/Editarepresentantes" + representantes.id}><FontAwesomeIcon icon={faEdit}/></Link>{' '}
+                                    <Button size="sm" variant="outline-danger" onClick={( ) => {this.deletar(representantes.id)}}><FontAwesomeIcon icon={faTrash}/></Button>{' '}                                  
+                                </ButtonGroup>
+                           </td>
+                       </tr> 
+                    ))
+                }
+                
+
+            </tbody>
+        </Table>
+    )
+
+
+
+
+
+    }
+}
